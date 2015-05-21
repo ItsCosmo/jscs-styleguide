aba72f7695f6707f28760e56e31282510624e57f
Paul Kleimeyer <falling.down@cox.net>
2015-05-21 12:24:54 -0700

    code cleanup, new api
    removed un-needed handlebars helpers
    added body() and html() methods, changed html() to file()

--- index.js
+++ index.js
@@ -5,80 +5,16 @@
     var fs = require("fs"),
         hb = require("handlebars"),
         main = require("./templates/main.hbs"),
-        attribute = require("./templates/attribute.hbs"),
+        body = require("./templates/body.hbs"),
+        rule = require("./templates/rule.hbs"),
         rules = require("./rules");
 
-    hb.registerPartial("attribute", attribute);
+    hb.registerPartial("body", body);
+    hb.registerPartial("rule", rule);
 
-    hb.registerHelper("ifTypeof", function(v1, v2, options) {
-        return (Object.prototype.toString.call(v1) === v2) ? options.fn(this) : options.inverse(this);
-    });
-    
-    hb.registerHelper("add", function(val, add) {
-        return val + add;    
-    });
-    
-    hb.registerHelper("ifCond", function(v1, operator, v2, options) {
-
-        switch (operator) {
-            case "==":
-                return (v1 == v2) ? options.fn(this) : options.inverse(this);
-            case "===":
-                return (v1 === v2) ? options.fn(this) : options.inverse(this);
-            case "<":
-                return (v1 < v2) ? options.fn(this) : options.inverse(this);
-            case "<=":
-                return (v1 <= v2) ? options.fn(this) : options.inverse(this);
-            case ">":
-                return (v1 > v2) ? options.fn(this) : options.inverse(this);
-            case ">=":
-                return (v1 >= v2) ? options.fn(this) : options.inverse(this);
-            case "&&":
-                return (v1 && v2) ? options.fn(this) : options.inverse(this);
-            case "||":
-                return (v1 || v2) ? options.fn(this) : options.inverse(this);
-            default:
-                return options.inverse(this);
-        }
-    });
-
-    hb.registerHelper("spaces", function(n) {
-        return Array(n + 1).join(" ");    
-    });
-    
-    // Adapted from article by Steen F. Tøttrup
-    // http://www.newsuntold.dk/post/dynamic-partials-with-handlebars
-    hb.registerHelper("partial", function(name , context, opts) {
-        name = name.replace(/\//g, '_');
-        var partial = hb.partials[name];
-        if (!partial) {
-            return "";
-        }
-        return new hb.SafeString(hb.compile(partial)(context));
-    });
-    
-    hb.registerHelper("othermark", function(mark) {
-        if (mark === '"') {
-            return "'";
-        }
-        if (mark === "'") {
-            return '"';
-        }
-        return "";
-    });
-    
-    function evaluate(str) {
-        var jscs = this.config;
-        return eval(str);
-    }
-
-    function spaces(n) {
-        return Array(n + 1).join(" ");
-    }
-    
     module.exports = {
-        html: function(jscs, options) {
-            var out = this.generateText(jscs, options);
+        file: function(jscs, options) {
+            var out = this.html(jscs, options);
 
             fs.unlink("styleguide.html", function(err) {
                 if (err) {
@@ -93,7 +29,7 @@
 
             return out;
         },
-        generateText: function(jscs, options) {
+        html: function(jscs, options) {
             var options = options || {},
                 props = {};
             options.title = options.title || "Style Guide";
@@ -106,6 +42,20 @@
             }
 
             return main({options: options, props: props});
+        }, 
+        body: function(jscs, options) {
+            var options = options || {},
+                props = {};
+            options.title = options.title || "Style Guide";
+            options.theme = options.theme || "default";
+            options.valid_keyword = options.valid_keyword || "VALID";
+            options.invalid_keyword = options.invalid_keyword || "INVALID";
+
+            for (rule in rules) {
+                props[rule] = rules[rule](jscs[rule]);
+            }
+            
+            return body({options: options, props: props});
         }
     };
 }());
